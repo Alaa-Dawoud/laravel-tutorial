636179157f7f5c0827ba7439bc39773652dea47a
Alaa-Dawoud <48125535+Alaa-Dawoud@users.noreply.github.com>
2024-02-28 10:43:37 +0200

    Add files via upload

---
+++ PizzaController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pizza;
+
+class PizzaController extends Controller
+{
+    public function index(){
+        // $pizzas = Pizza::all();
+        // $pizzas = Pizza::orderBy('name', 'desc')->get();
+        // $pizzas = Pizza::where(['type'=>'hawaiian', 'name'=>'ahmed'])->get();
+        // $pizzas = Pizza::where('type', 'hawaiian')->orderBy('name', 'desc')->get();
+        // $pizzas = Pizza::where('type','hawaiian')->get();
+        $pizzas = Pizza::all();
+        return view('pizzas.index', [
+            'pizzas'=>$pizzas,
+            'name'=>request('name'),
+            'age'=>request('age')
+        ]);
+    }
+    public function show($id){
+        $pizza = Pizza::findOrFail($id);
+        return view('pizzas.show', ['pizza'=>$pizza]);
+    }
+    public function create(){
+        return view('pizzas.create');
+    }
+}
